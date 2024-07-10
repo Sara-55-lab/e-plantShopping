b6a0595f7a5f5e83018c8fb1c884ead6b0fa00c9
sara-55-lab <sarroh71@gmail.com>
2024-07-10 14:47:17 -0400

    initial commit

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CreatSlice';
 
- const store = configureStore({
+const store = configureStore({
     reducer: {
+      cart: cartReducer, // Associate cartReducer with the 'cart' slice of the state
+      // Add more reducers if needed
     },
-});
+  });
 export default store
